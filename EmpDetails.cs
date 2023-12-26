f8841f3b97ef3ba1d2b03307534d0c267ff52f66
Omkar Godse <39644317+omkargodse@users.noreply.github.com>
2023-12-26 14:53:52 +0530

    Add CRUD app

---
+++ EmpDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos_CRUD
+{
+    public class EmpDetails
+    {
+        public Guid id { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string address { get; set; }
+        public string zipcode { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cosmos_CRUD;
+using Microsoft.Azure.Cosmos;
+
+Console.WriteLine("Hello, World!");
+
+string key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+string URI = "https://localhost:8081";
+
+CosmosClient client = new CosmosClient(URI, key);
+
+Database database = await client.CreateDatabaseIfNotExistsAsync("EmployeeMaster");
+
+Container container = await database.CreateContainerIfNotExistsAsync("EmpDetails", "/zipcode", 400);
+
+// Create item
+EmpDetails empDetails = new EmpDetails()
+{
+    id = Guid.NewGuid(),
+    address = "Pune",
+    fName = "XYZ",
+    lName = "ABC",
+    zipcode = "411019"
+};
+
+var x = await container.CreateItemAsync<EmpDetails>(empDetails);
+PartitionKey partition = new PartitionKey("411019");
+
+// Read item
+EmpDetails t = await container.ReadItemAsync<EmpDetails>("e2f35c47-010a-49b3-a55e-d14397f6b015", partition);
+
+
+// Update item
+t.address = "Mumbai";
+
+await container.UpsertItemAsync<EmpDetails>(t);
+Console.WriteLine(t.address);
+
+// Delete item
+var R = await container.DeleteItemAsync<EmpDetails>(t.id.ToString(), partition);
+// Console.WriteLine(x.ETag);
+//Console.WriteLine(x.RequestCharge);
+//Console.WriteLine(x.Headers);
+
+Console.ReadLine();
